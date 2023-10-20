6c8d7c23477df8d567d2315562a9bf5a8e8a457e
itzy11 <135247407+itzy11@users.noreply.github.com>
2023-10-19 18:08:32 -0600

    versión que funciona para graficar barras junto a árbol
    Hubo que cambiar el nombre de la columna Strain a label para que aplot pueda alinearlas correctamente

--- Numero_genes.R
+++ Numero_genes.R
@@ -1,16 +1,28 @@
 #Numero de genes 
-library(ggtree)
 library(ggplot2)
+library(dplyr)
 library(aplot)
+library(ggtree)
 
-tree <- read.tree("arbolito.newick")
-d <- read.csv('genoma_genes.csv')
+
+
+tree <- read.tree("../../arbol_filo/arbolito.newick")
+d <- read.csv('../../arbol_filo/genoma_genes.csv')
+d <- d %>%
+  mutate(Strain = stringr::str_replace_all(Strain, " ", "_")) %>%
+  rename(label = Strain)
 
 tree <- ggtree(tree)
 
-p1 <- ggplot(d, aes(genes, Strain)) + geom_col(aes(fill='blue')) + 
-  geom_text(aes(label= genes, y= Strain)) +
-  coord_trans()+ theme_classic() + theme(legend.position='none')
-
+p1 <- d %>%
+  ggplot(aes(y = label, x = genes)) +
+  geom_col(fill='blue') +
+  geom_text(aes(label = genes)) +
+  theme_classic() +
+  theme(axis.title.y = element_blank())
+p1
 p1 %>% insert_left(tree)
+
+
+
 
