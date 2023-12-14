fba2d60086339459391ad1bc5340cb12fe322129
itzyy <135247407+itzy11@users.noreply.github.com>
2023-12-14 14:27:04 -0600

    update Tamaño_genoma.R

--- Tamaño_genoma.R
+++ Tamaño_genoma.R
@@ -1,18 +1,23 @@
-#Numero de genes 
-library(ggtree)
+#Tamaño Genoma
 library(ggplot2)
+library(dplyr)
 library(aplot)
+library(ggtree)
+setwd("~/Documents/lab_sur/arbol_filo")
 
 tree <- read.tree("arbolito.newick")
 d <- read.csv('genoma_genes.csv')
+d <- d %>%
+  mutate(Strain = stringr::str_replace_all(Strain, " ", "_")) %>%
+  rename(label = Strain)
 
 tree <- ggtree(tree)
 
-d$Strain = as.factor(d$Strain)
-p1 <- ggplot(d, aes(genoma, Strain)) + geom_col(aes(fill='blue')) + 
-  geom_text(aes(label= genoma, y= Strain)) +
-  coord_trans() + theme_classic() + theme(legend.position='none')
-
+p1 <- d %>%
+  ggplot(aes(y = label, x = genoma)) +
+  geom_col(fill='blue') +
+  geom_text(aes(label = genoma)) +
+  theme_classic() +
+  theme(axis.title.y = element_blank())
+p1
 p1 %>% insert_left(tree)
-
-
